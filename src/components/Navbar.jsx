7c282cd6d014047598f140e149b6204bff2c2266
Simon Emmanuel <120514869+symon9@users.noreply.github.com>
2024-11-29 00:10:55 +0100

    Update Navbar ref

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
           navLinks.map( (nav, index) => (
             <li key={nav.id}
             className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'} text-white mr-10`}>
-              <a href={`${nav.id}`}>
+              <a href="#">
                 {nav.title}
               </a>
             </li>
